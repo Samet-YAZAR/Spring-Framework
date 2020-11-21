213f59b1611290e5fbb51d66ad873cd7942c05b4
yzrsamet <yzr.samet@gmail.com>
2020-11-22 00:20:52 +0100

    Imply Setter Injection instead of Dependency Injection Load Spring Framework

--- Sample-App - Java/src/main/java/AppConfig.java
+++ Sample-App - Java/src/main/java/AppConfig.java
@@ -1,3 +1,5 @@
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +10,26 @@ public class AppConfig {
     //configure Annotations.
     //@Configuration
 
-        @Bean(name="speakerservice")
+        @Bean(name="speakerService")
         public SpeakerService getSpeakerService(){
-            return new SpeakerServiceImpl();
+            SpeakerServiceImpl service = new SpeakerServiceImpl();
+            service.setRepository(getSpeakerRepository());
+            return service;
         }
-}
+
+        @Bean(name="speakerRepository")
+        public SpeakerRepository getSpeakerRepository(){
+            return new HibernateSpeakerRepositoryImpl();
+        }
+    } // bu altli ustlu yapiya Setter injection Diyoruz.
+
+
+
+
+/*
+* simdi dusunelim, repository paketi altinda bir speakerRepo var ama onu implement eden
+* 10 class olsun, bu 10 farkli Dependancy injection anlamina gelir, bu cok istedigimi
+* bir sey degil, aynisii service icinde dusunelim.
+* Biz burada sub classta service olarak reponun interfaceini field olarak kullanip setterini yazdik.
+*
+* */
--- Sample-App - Java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ Sample-App - Java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -8,12 +8,17 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
     //Dependency injection HardCoded Style of Maven
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    //private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    //to use our service we make DI to have access to the repo.
+    //but with spring no longer we need to do it.
+
+    private SpeakerRepository repository;
 
     public List<Speaker> findAll() {
         return repository.findAll();
     }
 
-
-
+    public void setRepository(SpeakerRepository repository) {
+        this.repository = repository;
+    }
 }
