700d677a036061cdf6de0ff5828832ac86b2b3ef
yzrsamet <yzr.samet@gmail.com>
2020-11-22 01:41:26 +0100

    Swith to Xml configuration of Spring

---
+++ Sample-App - XML/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -0,0 +1,19 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
+
+import java.util.List;
+
+public class SpeakerServiceImpl implements SpeakerService {
+    //Dependency injection HardCoded Style of Maven
+    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+
+    public List<Speaker> findAll() {
+        return repository.findAll();
+    }
+
+
+
+}
